74d91cff516bbc03d8b385a6e925de6d4ac77e1c
zamarrowski <sergiozamarro@hotmail.com>
2018-09-16 01:45:29 +0200

    Cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Alert } from './Alert/Alert.js'
 export { default as Badge } from './Badge/Badge.js'
 export { default as Button } from './Button/Button'
+export { default as Card } from './Card/Card'
