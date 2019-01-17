25ade6fdc69d628e37cc09978533e6e2478dd3ee
zamarrowski <sergiozamarro@hotmail.com>
2019-01-17 20:00:31 +0100

    Card doc

--- src/Card/Card.stories.js
+++ src/Card/Card.stories.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
+import { storiesOf } from '@storybook/react'
+import { withReadme } from 'storybook-readme'
 
 import Card from './Card'
-import Button from './../Button/Button';
+import Button from './../Button/Button'
+import Readme from './card.md'
 
 
 storiesOf('Card', module)
-  .add('simple', () => <Card title="My card">Content card</Card>)
-  .add('with actions', () => <Card title="My card" actions={<Button>Button</Button>}>Content card</Card>)
-  .add('with image', () => <Card image="https://vmware.github.io/clarity/assets/images/documentation/cards/placeholder_350x150.png" title="My card" actions={<Button>Button</Button>}>Content card</Card>)
+  .add('simple', withReadme(Readme, () => <Card title="My card">Content card</Card>))
+  .add('with actions', withReadme(Readme, () => <Card title="My card" actions={<Button>Button</Button>}>Content card</Card>))
+  .add('with image', withReadme(Readme, () => <Card image="https://vmware.github.io/clarity/assets/images/documentation/cards/placeholder_350x150.png" title="My card" actions={<Button>Button</Button>}>Content card</Card>))
---
+++ src/Card/card.md
@@ -0,0 +1,27 @@
+# Card component
+
+## Examples
+
+### Simple card
+
+```
+<Card title="My card">Content card</Card>
+```
+
+### Card with actions
+
+```
+<Card title="My card" actions={<Button>Button</Button>}>Content card</Card>
+```
+
+### Card with image
+
+```
+<Card 
+  image="https://vmware.github.io/clarity/assets/images/documentation/cards/placeholder_350x150.png" 
+  title="My card" 
+  actions={<Button>Button</Button>}
+>
+  Content card
+</Card>
+```
