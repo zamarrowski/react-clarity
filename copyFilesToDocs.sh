a57fa943502ddf111e45d8931f91a461aa3a42e7
zamarrowski <sergiozamarro@hotmail.com>
2019-01-22 12:48:52 +0100

    clean branch

--- copyFilesToDocs.sh
+++
@@ -1,7 +0,0 @@
-npm run prepublish
-rm -rf ../react-clarity-test/node_modules/react-clarity
-cp ./src/MetropolisRegular.otf ./lib/
-cp -r ./lib ../react-clarity-test/node_modules/react-clarity
-rm -rf ../react-clarity-test/node_modules/react-clarity/node_modules
-mkdir -p ../react-clarity-test/node_modules/react-clarity/node_modules/react-ionicons
-cp -r ./node_modules ../react-clarity-test/node_modules/react-clarity
--- src/Spinner/spinner.md
+++
@@ -1,4 +0,0 @@
-# Spinner component
-
-## Example
-`<Spinner/>`
